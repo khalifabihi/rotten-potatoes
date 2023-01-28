f2c211d58aca8e85e16c541c9645a8bb916d3f48
Khalifa <khalifa.bihi351@campus.vinci.ac.ma>
2023-01-28 21:13:06 +0100

    test

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +11,28 @@ export class LoginComponent implements OnInit {
 
   username = "";
   password = "";
+  errorMsg = "";
 
-  constructor() { }
+  constructor(private auth:AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    if (this.username.trim().length === 0 ) {
+      this.errorMsg = "Please enter a username";
+    } else if (this.password.trim().length === 0 ) {
+      this.errorMsg = "Please enter a password";
+    } else {
+      this.errorMsg = "";
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+        this.errorMsg = "Invalid username or password";
+      }
+    }
   }
 
 }
